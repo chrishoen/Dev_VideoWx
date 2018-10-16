00be6ee32b0b7a7e1400a79f86e9ce2893d06a3e
chrishoen <chris123hoen@gmail.com>
2018-10-16 11:42:49 -0400

    first commit

---
+++ Test/Source/CmdLineExec.cpp
@@ -0,0 +1,88 @@
+
+#include "stdafx.h"
+
+#include "CmdLineExec.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+CmdLineExec::CmdLineExec()
+{
+}
+
+void CmdLineExec::reset()
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Base class override. Execute a command line command. It calls one of
+// the following specific command execution functions. This is called by
+// the owner of this object to pass command line commands to it. 
+
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if(aCmd->isCmd("GO1"    ))  executeGo1(aCmd);
+   if(aCmd->isCmd("GO2"    ))  executeGo2(aCmd);
+   if(aCmd->isCmd("GO3"    ))  executeGo3(aCmd);
+   if(aCmd->isCmd("GO4"    ))  executeGo4(aCmd);
+   if(aCmd->isCmd("GO5"    ))  executeGo5(aCmd);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+   // Set defaults if no arguments were entered.
+   aCmd->setArgDefault(1,10);
+   aCmd->setArgDefault(2,11.1);
+
+   // Set variables from arguments.
+   int    tInt    = aCmd->argInt(1);
+   double tDouble = aCmd->argDouble(2);
+
+   // Show variables.
+   Prn::print(0,"Show2 %d %10.6f",tInt,tDouble);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+   // Set defaults if no arguments were entered.
+   aCmd->setArgDefault(1,"something");
+
+   // Show arguments.
+   Prn::print(0,"Go2 %s %10.6f",aCmd->argString(1));
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
+{
+}
+
