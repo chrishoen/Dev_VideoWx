3a9e6f0c3544a8d4e45ef2da5e924730f755f9d9
chrishoen <chris123hoen@gmail.com>
2018-10-16 12:09:34 -0400

    Video1 works with .bmp file

--- Video1/Source/Main.cpp
+++ Video1/Source/Main.cpp
@@ -1,11 +1,52 @@
 #include "stdafx.h"
 
+#include "SDL.h"
+
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 
 int main(int argc,char** argv)
 {
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize the program.
+
+   int running;
+   SDL_Window *window;
+   SDL_Surface *windowsurface;
+   SDL_Surface *image;
+   SDL_Event event;
+
+   SDL_Init(SDL_INIT_VIDEO);
+
+   window = SDL_CreateWindow("Hello World",
+      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+      592, 460, SDL_WINDOW_SHOWN);
+   windowsurface = SDL_GetWindowSurface(window);
+
+   image = SDL_LoadBMP("..\\Files\\sails.bmp");
+   SDL_BlitSurface(image, NULL, windowsurface, NULL);
+
+   SDL_UpdateWindowSurface(window);
+   SDL_FreeSurface(image);
+
+   running = 1;
+   while (running) {
+      while (SDL_PollEvent(&event) != 0) {
+         if (event.type == SDL_QUIT) {
+            running = 0;
+            break;
+         }
+      }
+   }
+   SDL_DestroyWindow(window);
+   SDL_Quit();
+   return 0;
+
+
+
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
