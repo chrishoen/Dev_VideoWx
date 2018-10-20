66666e9c025782b7d8677e222a177dd6292085fa
chrishoen <chris123hoen@gmail.com>
2018-10-20 10:17:26 -0400

    more VThread

---
+++ Files/aaa_vthread.bat
@@ -0,0 +1,5 @@
+start "VTHREAD" C:\Prime\DevelopFComm\Dev_VideoWx\x64\Release\VThread.exe
+
+
+
+
--- VThread/Source/MainInit.cpp
+++ VThread/Source/MainInit.cpp
@@ -15,28 +15,10 @@ void main_initialize(int argc,char** argv)
 
    // Initialize print facility.
    Prn::resetPrint();
-   Prn::useConsole(1);
+// Prn::useConsole(1);
    Prn::initializePrint();
 
    // Initialize print filters.
-   Prn::setFilter(Prn::SerialInitP1,    true);
-   Prn::setFilter(Prn::SerialInitP2,    true);
-   Prn::setFilter(Prn::SerialRxRunP1,   false);
-   Prn::setFilter(Prn::SerialRxRunP2,   false);
-   Prn::setFilter(Prn::SerialTxRunP1,   false);
-   Prn::setFilter(Prn::SerialTxRunP2,   false);
-   Prn::setFilter(Prn::SerialErrorP1,   true);
-   Prn::setFilter(Prn::SerialErrorP2,   true);
-
-   Prn::setFilter(Prn::SerialInitM1,    true);
-   Prn::setFilter(Prn::SerialInitM2,    true);
-   Prn::setFilter(Prn::SerialRxRunM1,   false);
-   Prn::setFilter(Prn::SerialRxRunM2,   false);
-   Prn::setFilter(Prn::SerialTxRunM1,   false);
-   Prn::setFilter(Prn::SerialTxRunM2,   false);
-   Prn::setFilter(Prn::SerialErrorM1,   true);
-   Prn::setFilter(Prn::SerialErrorM2,   true);
-
    Prn::setFilter(Prn::ThreadInit1,     true);
    Prn::setFilter(Prn::ThreadInit1,     true);
    Prn::setFilter(Prn::ThreadRun1,      true);
